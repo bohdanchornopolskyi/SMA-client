5534a27c49133c939a7076d55a5294ebca17b16c
Bohdan Chornopolskyi <bogdan.chornopolskiy28@gmail.com>
2021-02-02 11:34:13 +0200

    change uri

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -4,7 +4,7 @@ import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@ap
 import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-    uri: 'http://localhost:5000',
+    uri: 'https://sma-bohdan-chornopolskyi.herokuapp.com/',
 });
 
 const authLink = setContext(() => {
